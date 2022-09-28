4420443a498470c5f7b221e68b8fce134a8db227
SamPan24 <74392347+SamPan24@users.noreply.github.com>
2022-09-28 14:32:20 -0400

    changing hello.py for activity 3

--- hello.py
+++ hello.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="/Users/SamPan 1/Documents/ECE444/Lab2-Flask/lab2_venv")
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    #return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    #return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
