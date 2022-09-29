287beef4febbd147220b30237df093c1ad32442a
SamPan24 <74392347+SamPan24@users.noreply.github.com>
2022-09-28 23:00:57 -0400

    update hello.py for activity 4

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ from wtforms.validators import DataRequired
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField("What is your UofT Email address?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 app = Flask(__name__, template_folder="/Users/SamPan 1/Documents/ECE444/Lab2-Flask/lab2_venv")
@@ -21,13 +22,29 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name=session.get('name')
+        old_email=session.get('email')
+        
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you have changed your name!")
         session['name'] = form.name.data
+        
+        if old_email is not None and old_email != form.email.data:
+            flash("Looks like you have changed your email!")
+        session['email'] = form.email.data
+        
+        new_email = form.email.data
+        
+        if "utoronto" in form.email.data:
+            session["message"] = f"Your UofT email is {new_email}"
+        else:
+            session["message"] = "Please use an UofT email!"
+        
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get("name"), message=session.get("message"))
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+
