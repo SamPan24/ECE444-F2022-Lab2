9aa0b75cc58ef0c78842d4ecec18ea4edc03450a
SamPan24 <74392347+SamPan24@users.noreply.github.com>
2022-09-28 22:12:44 -0400

    updated hello.py to reproduce example 4-7 in textbook

--- hello.py
+++ hello.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 app = Flask(__name__, template_folder="/Users/SamPan 1/Documents/ECE444/Lab2-Flask/lab2_venv")
+app.config["SECRET_KEY"] = "abcdefg"
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    #return '<h1>Hello World!</h1>'
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name=session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
 def user(name):
-    #return '<h1>Hello, {}!</h1>'.format(name)
     return render_template('user.html', name=name, current_time=datetime.utcnow())
